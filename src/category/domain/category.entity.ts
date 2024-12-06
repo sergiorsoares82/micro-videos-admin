56306836f2899f0637b043f1402e9a9c04e145b5
sergiorsoares82 <sergio.veiga.soares@gmail.com>
2024-12-06 07:44:04 -0300

    adicionando uuid na entidade categoria

--- src/category/domain/category.entity.ts
+++ src/category/domain/category.entity.ts
@@ -1,5 +1,7 @@
+import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
+
 export type CategoryConstructorProps = {
-  category_id?: string;
+  category_id?: Uuid;
   name: string;
   description?: string | null;
   is_active?: boolean;
@@ -11,13 +13,13 @@ export type CategoryCreateCommand = {
   is_active?: boolean;
 };
 export class Category {
-  category_id: string;
+  category_id: Uuid;
   name: string;
   description: string | null;
   is_active: boolean;
   created_at: Date;
   constructor(props: CategoryConstructorProps) {
-    this.category_id = props.category_id;
+    this.category_id = props.category_id ?? new Uuid();
     this.name = props.name;
     this.description = props.description ?? null;
     this.is_active = props.is_active ?? true;
@@ -41,7 +43,7 @@ export class Category {
   }
   toJSON() {
     return {
-      category_id: this.category_id,
+      category_id: this.category_id.id,
       name: this.name,
       description: this.description,
       is_active: this.is_active,
