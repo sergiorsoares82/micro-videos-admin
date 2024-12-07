c37e881bba3b7be524ec3ba47c3a92e1cc99cc1c
sergiorsoares82 <sergio.veiga.soares@gmail.com>
2024-12-07 09:12:09 -0300

    criando classe de validação para categoria

---
+++ src/shared/domain/validators/validator-fields-interface.ts
@@ -0,0 +1,8 @@
+export type FieldsErrors = {
+  [field: string]: string[];
+};
+export interface IValidatorFields<PropsValidated> {
+  errors: FieldsErrors | null;
+  validatedData: PropsValidated | null;
+  validate(data: any): boolean;
+}
