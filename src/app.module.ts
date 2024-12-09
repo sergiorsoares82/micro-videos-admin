08b7559f01fba73752a7a77df0a2679df172a99a
sergiorsoares82 <sergio.veiga.soares@gmail.com>
2024-12-09 20:52:52 +0000

    Integração do Nest.js com Sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
 
 @Module({
-  imports: [CategoriesModule],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      host: ":memory:",
+      logging: false,
+      models: [CategoryModel],
+    }),
+    CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import type { CategorySequelizeRepository } from '@core/category/infra/db/sequelize/category-sequelize.repository';
 
 @Controller('categories')
 export class CategoriesController {
+  constructor(private categoryRepo: CategorySequelizeRepository){
+    console.log(this.categoryRepo);
+  }
 
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
