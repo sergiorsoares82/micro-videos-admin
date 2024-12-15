2b976ced179ae42bde0b66500e7d6e4b2cc94380
sergiorsoares82 <sergio.veiga.soares@gmail.com>
2024-12-15 19:00:04 +0000

    Criando end-to-end para criação de categorias

--- test/categories/create-category.e2e-spec.ts
+++ test/categories/create-category.e2e-spec.ts
@@ -7,6 +7,10 @@ import { CATEGORY_PROVIDERS } from '../../src/nest-modules/categories-module/cat
 import { AppModule } from '../../src/app.module';
 import { applyGlobalConfig } from '../../src/nest-modules/global-config';
 import { startApp } from '../../src/nest-modules/shared-module/testing/helpers';
+import { Uuid } from '@core/shared/domain/value-objects/uuid.vo';
+import { CategoriesController } from 'src/nest-modules/categories-module/categories.controller';
+import { CategoryOutputMapper } from '@core/category/application/common/category-output';
+import { instanceToPlain } from 'class-transformer';
 
 describe('CategoriesController (e2e)', () => {
   const appHelper = startApp();
@@ -19,18 +23,60 @@ describe('CategoriesController (e2e)', () => {
   });
   
   describe('/categories (POST)', () => {
-    const arrange = CreateCategoryFixture.arrangeForCreate();
+    describe('should return a response error with 422 status code when request body is invalid', () => {
+      const invalidRequest = CreateCategoryFixture.arrangeInvalidRequest();
+      const arrange = Object.keys(invalidRequest).map((key) => ({
+        label: key,
+        value: invalidRequest[key],
+      }));
     
-    test.each(arrange)(
-      'when body is $send_data',
-      async ({ send_data, expected }) => {
-        const res = await request(appHelper.app.getHttpServer())
-          .post('/categories')
-          .send(send_data)
-          .expect(201);
-        const keysInResponse = CreateCategoryFixture.keysInResponse;
-        expect(Object.keys(res.body)).toStrictEqual(['data']);
-      },
-    );
+      test.each(arrange)('when body is $label', ({ value }) => {
+        return request(appHelper.app.getHttpServer())
+          .post('/categories').send(value.send_data)
+          .expect(422)
+          .expect(value.expected);
+        });
+      });
+      describe('should return a response error with 422 status code when throw EntityValidationError', () => {
+        const invalidRequest =
+          CreateCategoryFixture.arrangeForEntityValidationError();
+        const arrange = Object.keys(invalidRequest).map((key) => ({
+          label: key,
+          value: invalidRequest[key],
+        }));
+        test.each(arrange)('when body is $label', ({ value }) => {
+          return request(appHelper.app.getHttpServer())
+            .post('/categories')
+            .send(value.send_data)
+            .expect(422)
+            .expect(value.expected);
+        });
+      });
+      describe('should create a category', () => {
+        const arrange = CreateCategoryFixture.arrangeForCreate();
+        test.each(arrange)(
+          'when body is $send_data',
+          async ({ send_data, expected }) => {
+            const res = await request(appHelper.app.getHttpServer())
+              .post('/categories')
+              .send(send_data)
+              .expect(201);
+            const keysInResponse = CreateCategoryFixture.keysInResponse;
+            expect(Object.keys(res.body)).toStrictEqual(['data']);
+            expect(Object.keys(res.body.data)).toStrictEqual(keysInResponse);
+            const id = res.body.data.id;
+            const categoryCreated = await categoryRepo.findById(new Uuid(id));
+            const presenter = CategoriesController.serializeCategory(
+              CategoryOutputMapper.toOutput(categoryCreated),
+            );
+            const serialized = instanceToPlain(presenter);
+            expect(res.body.data).toStrictEqual({
+              id: serialized.id,
+              created_at: serialized.created_at,
+              ...expected,
+            });
+          },
+        );
+      });
   });
 });
