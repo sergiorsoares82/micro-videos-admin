e712ff52b412ceef9c320ed54993e4ff25e00d9b
sergiorsoares82 <sergio.veiga.soares@gmail.com>
2024-12-15 17:13:50 +0000

    Fechando testes de integração do controller de categoria

--- src/core/category/application/use-cases/update-category/update-category.use-case.ts
+++ src/core/category/application/use-cases/update-category/update-category.use-case.ts
@@ -1,13 +1,9 @@
-import { IUseCase } from "../../../../shared/application/use-case.interface";
-import { NotFoundError } from "../../../../shared/domain/errors/not-found.error";
-import { EntityValidationError } from "../../../../shared/domain/validators/validation.error";
-import { Uuid } from "../../../../shared/domain/value-objects/uuid.vo";
-import { Category } from "../../../domain/category.entity";
-import { ICategoryRepository } from "../../../domain/category.repository";
-import {
-  CategoryOutput,
-  CategoryOutputMapper,
-} from "../../common/category-output";
+import { IUseCase } from '../../../../shared/application/use-case.interface';
+import { NotFoundError } from '../../../../shared/domain/errors/not-found.error';
+import { EntityValidationError } from '../../../../shared/domain/validators/validation.error';
+import { Uuid } from '../../../../shared/domain/value-objects/uuid.vo';
+import { Category } from '../../../domain/category.entity';
+import { ICategoryRepository } from '../../../domain/category.repository';
 
 export class UpdateCategoryUseCase
   implements IUseCase<UpdateCategoryInput, UpdateCategoryOutput>
@@ -21,8 +17,10 @@ export class UpdateCategoryUseCase
     if (!category) {
       throw new NotFoundError(input.id, Category);
     }
-    input.name && category.changeName(input.name);
-    if ("description" in input) {
+    if (input.name) {
+      category.changeName(input.name);
+    }
+    if (input.description !== undefined) {
       category.changeDescription(input.description);
     }
     if (input.is_active === true) {
--- src/nest-modules/categories-module/__tests__/categories.controller.int-spec.ts
+++ src/nest-modules/categories-module/__tests__/categories.controller.int-spec.ts
@@ -10,6 +10,18 @@ import { UpdateCategoryUseCase } from '../../../core/category/application/use-ca
 import { ListCategoriesUseCase } from '../../../core/category/application/use-cases/list-categories/list-categories.use-case';
 import { GetCategoryUseCase } from '../../../core/category/application/use-cases/get-category/get-category.use-case';
 import { DeleteCategoryUseCase } from '../../../core/category/application/use-cases/delete-category/delete-category.use-case';
+import { Category } from '@core/category/domain/category.entity';
+import { CategoryOutputMapper } from '@core/category/application/common/category-output';
+import {
+  CategoryCollectionPresenter,
+  CategoryPresenter,
+} from '../categories.presenter';
+import { Uuid } from '@core/shared/domain/value-objects/uuid.vo';
+import {
+  CreateCategoryFixture,
+  ListCategoriesFixture,
+  UpdateCategoryFixture,
+} from '../testing/category-fixture';
 describe('CategoriesController Integration Tests', () => {
   let controller: CategoriesController;
   let repository: ICategoryRepository;
@@ -22,6 +34,7 @@ describe('CategoriesController Integration Tests', () => {
       CATEGORY_PROVIDERS.REPOSITORIES.CATEGORY_REPOSITORY.provide,
     );
   });
+
   it('should be defined', () => {
     expect(controller).toBeDefined();
     expect(controller['createUseCase']).toBeInstanceOf(CreateCategoryUseCase);
@@ -30,8 +43,119 @@ describe('CategoriesController Integration Tests', () => {
     expect(controller['getUseCase']).toBeInstanceOf(GetCategoryUseCase);
     expect(controller['deleteUseCase']).toBeInstanceOf(DeleteCategoryUseCase);
   });
-  it('should create a category', () => {});
-  it('should update a category', () => {});
+
+  describe('should create a category', () => {
+    const arrange = CreateCategoryFixture.arrangeForCreate();
+    test.each(arrange)(
+      'when body is $send_data',
+      async ({ send_data, expected }) => {
+        const presenter = await controller.create(send_data);
+        const entity = await repository.findById(new Uuid(presenter.id));
+        expect(entity.toJSON()).toStrictEqual({
+          category_id: presenter.id,
+          created_at: presenter.created_at,
+          ...expected,
+        });
+        const output = CategoryOutputMapper.toOutput(entity);
+        expect(presenter).toEqual(new CategoryPresenter(output));
+      },
+    );
+  });
+
+  describe('should update a category', () => {
+    const arrange = UpdateCategoryFixture.arrangeForUpdate();
+    const category = Category.fake().aCategory().build();
+    beforeEach(async () => {
+      await repository.insert(category);
+    });
+    test.each(arrange)(
+      'when body is $send_data',
+      async ({ send_data, expected }) => {
+        const presenter = await controller.update(
+          category.category_id.id,
+          send_data,
+        );
+        const entity = await repository.findById(new Uuid(presenter.id));
+        expect(entity.toJSON()).toStrictEqual({
+          category_id: presenter.id,
+          created_at: presenter.created_at,
+          name: expected.name ?? category.name,
+          description:
+            'description' in expected
+              ? expected.description
+              : category.description,
+          is_active:
+            expected.is_active === true || expected.is_active === false
+              ? expected.is_active
+              : category.is_active,
+        });
+        const output = CategoryOutputMapper.toOutput(entity);
+        expect(presenter).toEqual(new CategoryPresenter(output));
+      },
+    );
+  });
+
+  it('should delete a category', async () => {
+    const category = Category.fake().aCategory().build();
+    await repository.insert(category);
+    const response = await controller.remove(category.category_id.id);
+    expect(response).not.toBeDefined();
+    await expect(repository.findById(category.category_id)).resolves.toBeNull();
+  });
+
+  it('should get a category', async () => {
+    const category = Category.fake().aCategory().build();
+    await repository.insert(category);
+    const presenter = await controller.findOne(category.category_id.id);
+    expect(presenter.id).toBe(category.category_id.id);
+    expect(presenter.name).toBe(category.name);
+    expect(presenter.description).toBe(category.description);
+    expect(presenter.is_active).toBe(category.is_active);
+    expect(presenter.created_at).toStrictEqual(category.created_at);
+  });
+
+  describe('search method', () => {
+    describe('should sorted categories by created_at', () => {
+      const { entitiesMap, arrange } =
+        ListCategoriesFixture.arrangeIncrementedWithCreatedAt();
+      beforeEach(async () => {
+        await repository.bulkInsert(Object.values(entitiesMap));
+      });
+      test.each(arrange)(
+        'when send_data is $send_data',
+        async ({ send_data, expected }) => {
+          const presenter = await controller.search(send_data);
+          const { entities, ...paginationProps } = expected;
+          expect(presenter).toEqual(
+            new CategoryCollectionPresenter({
+              items: entities.map(CategoryOutputMapper.toOutput),
+              ...paginationProps.meta,
+            }),
+          );
+        },
+      );
+    });
+
+    describe('should return categories using pagination, sort and filter', () => {
+      const { entitiesMap, arrange } = ListCategoriesFixture.arrangeUnsorted();
+      beforeEach(async () => {
+        await repository.bulkInsert(Object.values(entitiesMap));
+      });
+      test.each(arrange)(
+        'when send_data is $send_data',
+        async ({ send_data, expected }) => {
+          const presenter = await controller.search(send_data);
+          const { entities, ...paginationProps } = expected;
+          expect(presenter).toEqual(
+            new CategoryCollectionPresenter({
+              items: entities.map(CategoryOutputMapper.toOutput),
+              ...paginationProps.meta,
+            }),
+          );
+        },
+      );
+    });
+  });
 });
 //agente viu este tipo de teste no curso
 //end to end
