9d81fd025bd27e70de1583302f48c4d4900d13cd
sergiorsoares82 <sergio.veiga.soares@gmail.com>
2024-12-15 17:59:45 +0000

    Tratamento de erros 404 e de validação de domínio

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { WrapperDataInterceptor } from './nest-modules/shared-module/interceptors/wrapper-data/wrapper-data.interceptor';
+import { NotFoundErrorFilter } from './nest-modules/shared-module/not-found/not-found-error.filter';
+import { EntityValidationErrorFilter } from './nest-modules/shared-module/not-found/entity-validation-error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +15,8 @@ async function bootstrap() {
   );
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalInterceptors(new WrapperDataInterceptor());
+  app.useGlobalFilters(new NotFoundErrorFilter(), new EntityValidationErrorFilter());
 
   await app.listen(process.env.PORT ?? 3000);
 }
